6ecf5cb71cfa69c0f707ea6675808592146386bd
nawe1321 <66645143+nawe1321@users.noreply.github.com>
2023-06-14 10:58:10 -0700

    Create run_check.sh

---
+++ run_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Detect the operating system
+os="$(uname -s)"
+
+if [[ "${os}" == "Darwin" ]]; then  # Mac OS
+    # Install dependencies
+    pip install -r requirements_mac.txt
+    # Run the Python script
+    python check_mac.py
+elif [[ "${os}" == "MINGW"* ]] || [[ "${os}" == "MSYS"* ]] || [[ "${os}" == "Windows"* ]]; then  # Windows
+    # Install dependencies
+    pip install -r requirements_windows.txt
+    # Run the Python script
+    python check_windows.py
+else
+    echo "Unsupported operating system: ${os}"
+fi
