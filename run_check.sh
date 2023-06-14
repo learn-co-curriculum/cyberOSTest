becadc0271f95dd818b27bf414f5f6e33f454491
nawe1321 <66645143+nawe1321@users.noreply.github.com>
2023-06-14 12:26:02 -0700

    Update run_check.sh

--- run_check.sh
+++ run_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://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@github.com/learn-co-curriculum/cyberOSTest.git
+git clone https://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@github.com/learn-co-curriculum/cyberOSTest.git && cd cyberOSTest
 # Detect the operating system
 os="$(uname -s)"
 
